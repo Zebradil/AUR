adc11af1c2ecfd6ab9bbce88ee41c0471e7a9ee7
German Lashevich <german.lashevich@gmail.com>
2023-11-06 01:15:05 +0100

    feat: add function to fetch the latest github release

--- scripts/lib/pkgbuild.sh
+++ scripts/lib/pkgbuild.sh
@@ -7,3 +7,10 @@ pkgb:header() {
 # shellcheck disable=SC2034,SC2154
 EOF
 }
+
+pkgb:get_latest_github_release() {
+curl -fsSL \
+    "https://api.github.com/repos/zebradil/$1/releases/latest" \
+    --header "Authorization: Bearer ${GITHUB_TOKEN:?}" \
+    | jq -r '.tag_name'
+}
