b25e168d41fce6ac9b2f70933e2af1e86712d005
German Lashevich <german.lashevich@gmail.com>
2023-11-07 10:12:12 +0100

    fix(python-powerline-taskwarrior): update hashsums

--- packages/python-powerline-taskwarrior/pkgbuild_data.sh
+++ packages/python-powerline-taskwarrior/pkgbuild_data.sh
@@ -9,7 +9,8 @@ url="https://github.com/Zebradil/powerline-taskwarrior"
 depends=(python3 powerline)
 makedepends=(python-build python-installer python-wheel)
 source=("https://files.pythonhosted.org/packages/source/${_name::1}/$_name/$_name-$pkgver.tar.gz")
-sha512sums=('c0bda3991225f67b6fd98c691fc01b671445afb6c85c8e58833008e489476dfce1b70418cd2ad4530e2a6e7093a8b16e7f72014eaf3ce9d14dc45b09e0eddc49')
+sha256sums=()
+update_hashsums=true
 
 build() {
     cd "$_name-$pkgver" || exit 1
