aac7306c3845662d87c334f97f43f9f5786fb4db
German Lashevich <german.lashevich@gmail.com>
2023-11-11 23:03:50 +0100

    feat(rustotpony-bin): add package

---
+++ packages/rustotpony-bin/pkgbuild_data.sh
@@ -0,0 +1,21 @@
+# shellcheck disable=SC2034,SC2154
+
+pkgname=rustotpony-bin
+_repo="zebradil/${pkgname%-bin}"
+_binname=totp
+provides=($_binname)
+pkgver="$(pkgb:get_latest_github_release "$_repo")"
+pkgdesc='RusTOTPony — CLI manager of one-time password generators like Google Authenticator'
+pkgrel=2
+conflicts=('rustotpony')
+arch=('x86_64')
+url="https://github.com/$_repo"
+license=('MIT')
+source=("${pkgname}-${pkgver}::https://github.com/$_repo/releases/download/${pkgver}/$_binname-linux")
+sha256sums=('SKIP')
+update_hashsums=true
+
+package() {
+    _binname=totp
+    install -Dm755 "${srcdir}/${_binname}-linux" "${pkgdir}/usr/bin/${_binname}"
+}
