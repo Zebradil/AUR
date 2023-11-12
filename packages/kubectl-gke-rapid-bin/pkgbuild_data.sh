f3d8b2cac07baa9daaa43d562c3a3063e71ce8f1
German Lashevich <german.lashevich@gmail.com>
2023-11-12 01:54:57 +0100

    feat(kubectl-gke): add packages

---
+++ packages/kubectl-gke-rapid-bin/pkgbuild_data.sh
@@ -0,0 +1,5 @@
+# shellcheck disable=SC2034,SC2154
+
+CHANNEL=rapid
+
+source "${LIB_DIR:?}/custom/kubectl_gke.sh"
---
+++ packages/kubectl-gke-stable-bin/pkgbuild_data.sh
@@ -0,0 +1,5 @@
+# shellcheck disable=SC2034,SC2154
+
+CHANNEL=stable
+
+source "${LIB_DIR:?}/custom/kubectl_gke.sh"
