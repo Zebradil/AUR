56dcf5dd6fdf057e149e4723e82de52123cbe723
German Lashevich <german.lashevich@gmail.com>
2023-11-13 01:38:48 +0100

    fix(python-powerline-taskwarrior): add poetry as make dependency

--- packages/python-powerline-taskwarrior/pkgbuild_data.sh
+++ packages/python-powerline-taskwarrior/pkgbuild_data.sh
@@ -6,8 +6,16 @@ pkgver=2.0.0
 pkgdesc="Powerline segment for showing information from Taskwarrior task manager"
 license=(MIT)
 url="https://github.com/Zebradil/powerline-taskwarrior"
-depends=(python3 powerline)
-makedepends=(python-build python-installer python-wheel)
+depends=(
+    powerline
+    python3
+)
+makedepends=(
+    python-build
+    python-installer
+    python-poetry
+    python-wheel
+)
 source=("https://files.pythonhosted.org/packages/source/${_name::1}/$_name/$_name-$pkgver.tar.gz")
 sha256sums=('SKIP')
 _z_update_hashsums=true
