eb15ea8e1d54088689a20c8a5e1959207d52c335
German Lashevich <german.lashevich@gmail.com>
2023-11-16 00:11:25 +0100

    feat(kbld): add package

---
+++ packages/kbld/pkgbuild_data.sh
@@ -0,0 +1,45 @@
+# shellcheck shell=bash disable=SC2034,SC2154
+
+pkgname=kbld
+_github_repo="carvel-dev/$pkgname"
+
+_gh_info=$(pkgb:get_github_info "$_github_repo")
+if [[ -n "$_gh_info" ]]; then
+    eval "$_gh_info"
+    pkgver="${pkgver#v}"
+    url="${url:-$_github_repo_url}"
+else
+    log::error "Failed to get info from GitHub"
+fi
+
+provides=("${pkgname}")
+makedepends=(bash go)
+source=("${pkgname}-${pkgver}::https://github.com/$_github_repo/archive/v${pkgver}.tar.gz")
+sha256sums=('')
+_z_update_hashsums=true
+
+prepare() {
+    cd "$pkgname-$pkgver"
+
+    # Overwrite LATEST_GIT_TAG to avoid detecting the latest tag from the wrong repo
+    sed -i "/^LATEST_GIT_TAG=/c\\LATEST_GIT_TAG=$pkgver" ./hack/build.sh
+}
+
+build() {
+    cd "$pkgname-$pkgver" || exit 1
+    export CGO_CPPFLAGS="${CPPFLAGS}"
+    export CGO_CFLAGS="${CFLAGS}"
+    export CGO_CXXFLAGS="${CXXFLAGS}"
+    export CGO_LDFLAGS="${LDFLAGS}"
+    export GOFLAGS="-buildmode=pie -trimpath -ldflags=-linkmode=external -mod=readonly -modcacherw"
+
+    ./hack/build.sh "$pkgver"
+}
+
+package() {
+    cd "$pkgname-$pkgver" || exit 1
+
+    BIN=$pkgname
+
+    install -Dm755 $BIN -t "$pkgdir/usr/bin"
+}
